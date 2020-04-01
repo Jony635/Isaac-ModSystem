7ead61e01394e8d72056ee4dfc09fbf9e0d1e7b8
Jonathan Molina <jony635molina@gmail.com>
2020-04-01 12:51:35 +0200

    generating room layout

---
+++ Isaac_ModSystem/Assets/Scripts/DDArray.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class DDArray
+{
+    private System.Object[][] array;
+
+    public DDArray()
+    {
+
+    }
+}
--- Isaac_ModSystem/Assets/Scripts/RoomManager.cs
+++ Isaac_ModSystem/Assets/Scripts/RoomManager.cs
@@ -15,46 +15,147 @@ public class RoomManager : MonoBehaviour
     public Room currentRoom;
 
     public Room initialRoom;
-    public List<Room> rooms = new List<Room>();
 
     private Camera mainCamera;
+    private List<Room> rooms = new List<Room>();
+
+    [Header("Room COnfiguration")]
+    public int amountRooms = 6;
+    float chanceToGetARoom = 0.1f;
+
 
     private void Awake()
     {
         Instance = this;
         currentRoom = initialRoom;
         mainCamera = Camera.main;
+        rooms.Add(initialRoom);
+
+        GenerateRoomLayout();
+    }
+
+    private void GenerateRoomLayout()
+    {
+        uint generatedRooms = 0;
+
+        while(generatedRooms != amountRooms)
+        {
+            int storedRooms = rooms.Count;
+            for (int i = 0; i < storedRooms; ++i)
+            {
+                Room room = rooms[i];
+                foreach (Door door in room.doors)
+                {
+                    float random = UnityEngine.Random.Range(0f, 1f);
+                    if (random <= chanceToGetARoom)
+                    {
+                        if (door.connectedDoor != null)
+                            continue;
+
+                        generatedRooms++;
+
+                        Vector3 newRoomPosition = Vector3.zero;
+
+                        switch (door.doorLocation)
+                        {
+                            case DoorPosition.TOP:
+                                {
+                                    newRoomPosition = door.thisRoom.transform.position + new Vector3(0, 10.36f, 0);
+                                    break;
+                                }
+
+                            case DoorPosition.BOTTOM:
+                                {
+                                    newRoomPosition = door.thisRoom.transform.position + new Vector3(0, -10.36f, 0);
+                                    break;
+                                }
+                            case DoorPosition.LEFT:
+                                {
+                                    newRoomPosition = door.thisRoom.transform.position + new Vector3(-15.36f, 0, 0);
+                                    break;
+                                }
+                            case DoorPosition.RIGHT:
+                                {
+                                    newRoomPosition = door.thisRoom.transform.position + new Vector3(15.36f, 0, 0);
+                                    break;
+                                }
+                            default:
+                                break;
+                        }
+                     
+                        GameObject newRoom = Instantiate(roomPrefab, newRoomPosition, Quaternion.identity, roomsContainer.transform);
+
+                        rooms.Add(newRoom.GetComponent<Room>());
+                        storedRooms++;
+
+                        switch (door.doorLocation)
+                        {
+                            case DoorPosition.TOP:
+                                {
+                                    Door newDoor = newRoom.GetComponent<Room>().doors[(int)DoorPosition.BOTTOM];
+                                    door.connectedDoor = newDoor;
+                                    newDoor.connectedDoor = door;
+                                    break;
+                                }
+
+                            case DoorPosition.BOTTOM:
+                                {
+                                    Door newDoor = newRoom.GetComponent<Room>().doors[(int)DoorPosition.TOP];
+                                    door.connectedDoor = newDoor;
+                                    newDoor.connectedDoor = door; break;
+                                }
+                            case DoorPosition.LEFT:
+                                {
+                                    Door newDoor = newRoom.GetComponent<Room>().doors[(int)DoorPosition.RIGHT];
+                                    door.connectedDoor = newDoor;
+                                    newDoor.connectedDoor = door; break;
+                                }
+                            case DoorPosition.RIGHT:
+                                {
+                                    Door newDoor = newRoom.GetComponent<Room>().doors[(int)DoorPosition.LEFT];
+                                    door.connectedDoor = newDoor;
+                                    newDoor.connectedDoor = door; break;
+                                }
+                            default:
+                                break;
+                        }
+
+                        door.UnLockDoor();
+                        door.connectedDoor.UnLockDoor();
+
+                        if (generatedRooms == amountRooms)
+                            return;
+                    }               
+                }
+            }
+
+        }
     }
 
     public void DoorTrespassed(Door door)
     {
-        Vector3 newRoomPosition = Vector3.zero;
         Vector3 characterOffset = Vector3.zero;
 
-        switch(door.doorLocation)
+        switch (door.doorLocation)
         {
             case DoorPosition.TOP:
                 {
-                    newRoomPosition = door.thisRoom.transform.position + new Vector3(0, 10.36f, 0);
                     characterOffset = new Vector3(0, 0.7f, 0);
                     break;
                 }
 
             case DoorPosition.BOTTOM:
                 {
-                    newRoomPosition = door.thisRoom.transform.position + new Vector3(0, -10.36f, 0);
                     characterOffset = new Vector3(0, -0.7f, 0);
                     break;
                 }
             case DoorPosition.LEFT:
                 {
-                    newRoomPosition = door.thisRoom.transform.position + new Vector3(-15.36f, 0, 0);
                     characterOffset = new Vector3(-0.7f, 0, 0);
                     break;
                 }
             case DoorPosition.RIGHT:
                 {
-                    newRoomPosition = door.thisRoom.transform.position + new Vector3(15.36f, 0, 0);
                     characterOffset = new Vector3(0.7f, 0, 0);
                     break;
                 }
@@ -62,57 +163,19 @@ public class RoomManager : MonoBehaviour
                 break;
         }
 
-        #region INSTANTIATE NEW ROOM
-        if(door.connectedDoor == null)
-        {
-            GameObject newRoom = Instantiate(roomPrefab, newRoomPosition, Quaternion.identity, roomsContainer.transform);
+        //door.connectedDoor.thisRoom.gameObject.SetActive(true);
 
-            switch (door.doorLocation)
-            {
-                case DoorPosition.TOP:
-                    {
-                        Door newDoor = newRoom.GetComponent<Room>().doors[(int)DoorPosition.BOTTOM];
-                        door.connectedDoor = newDoor;
-                        newDoor.connectedDoor = door;
-                        break;
-                    }
-
-                case DoorPosition.BOTTOM:
-                    {
-                        Door newDoor = newRoom.GetComponent<Room>().doors[(int)DoorPosition.TOP];
-                        door.connectedDoor = newDoor;
-                        newDoor.connectedDoor = door; break;
-                    }
-                case DoorPosition.LEFT:
-                    {
-                        Door newDoor = newRoom.GetComponent<Room>().doors[(int)DoorPosition.RIGHT];
-                        door.connectedDoor = newDoor;
-                        newDoor.connectedDoor = door; break;
-                    }
-                case DoorPosition.RIGHT:
-                    {
-                        Door newDoor = newRoom.GetComponent<Room>().doors[(int)DoorPosition.LEFT];
-                        door.connectedDoor = newDoor;
-                        newDoor.connectedDoor = door; break;
-                    }
-                default:
-                    break;
-            }
-        }
-        #endregion
-
-        #region CAMERA TRANSITION
         PlayerController.Instance.enabled = false;
 
         PlayerController.Instance.transform.position = door.connectedDoor.transform.position + characterOffset;
         PlayerController.Instance.ResetBodyAnimator();
 
         //Camera.main.transform.position = new Vector3(newRoomPosition.x, newRoomPosition.y, -1);
-        StartCoroutine(CameraTranslation(mainCamera, new Vector3(newRoomPosition.x, newRoomPosition.y, -1), 0.1f, () =>
+        StartCoroutine(CameraTranslation(mainCamera, new Vector3(door.connectedDoor.thisRoom.transform.position.x, door.connectedDoor.thisRoom.transform.position.y, -1), 0.1f, () =>
         {
+            //door.thisRoom.gameObject.SetActive(false);
             PlayerController.Instance.enabled = true;
         }));
-        #endregion
 
         currentRoom = door.connectedDoor.thisRoom;
     }
@@ -120,16 +183,12 @@ public class RoomManager : MonoBehaviour
     private IEnumerator CameraTranslation(Camera camera, Vector3 desiredPosition, float time, Action callback)
     {
         Vector3 currentVelocity = Vector3.zero;
-        Vector3 initialPos = camera.transform.position;
 
         while (camera.transform.position != desiredPosition)
         {
-            camera.transform.position = Vector3.SmoothDamp(camera.transform.position, desiredPosition, ref currentVelocity, time);
-            
-            if(camera.transform.position != desiredPosition)
-                yield return null;
+            camera.transform.position = Vector3.SmoothDamp(camera.transform.position, desiredPosition, ref currentVelocity, time);      
+            yield return null;
         }
-
         callback.Invoke();
-    }
+    }  
 }
